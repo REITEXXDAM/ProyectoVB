d815f26ac04bb2b6d3d29abf995f8a4444e31238
Iñigo_Dam <inigo.retamero@ikasle.egibide.org>
2024-01-10 21:08:21 +0100

    lo que faltaba

--- ProyectoVB/ProyectoVB/Modelo/Contacto.vb
+++ ProyectoVB/ProyectoVB/Modelo/Contacto.vb
@@ -1,3 +1,19 @@
 ﻿Public Class Contacto
 
+    Public Property nombre As String
+    Public Property apellido As String
+
+    Public Property telefono As Integer
+    Public Property email As String
+
+    Public Sub New()
+    End Sub
+
+    Public Sub New(nombre As String, apellido As String, telefono As Integer, email As String)
+
+        Me.nombre = nombre
+        Me.apellido = apellido
+        Me.telefono = telefono
+        Me.email = email
+    End Sub
 End Class
