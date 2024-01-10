a934491ab3baee62bd953911aea95aaa34abbd62
Iñigo_Dam <inigo.retamero@ikasle.egibide.org>
2024-01-10 19:57:46 +0100

    MEJORAMOS LOGIN, SE CARGA AL PULSAR BOTON

--- ProyectoVB/ProyectoVB/Vista/Login.vb
+++ ProyectoVB/ProyectoVB/Vista/Login.vb
@@ -3,12 +3,8 @@
 
     Dim command As New SQLiteCommand()
 
-    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' Llama al método ConsultarUsuarios y pasa la lista de usuarios al controlador
-        command.ConsultarUsuarios(controller.listaUsuarios)
-    End Sub
-
     Private Sub loginButton_Click(sender As Object, e As EventArgs) Handles loginButton.Click
+        command.ConsultarUsuarios(controller.listaUsuarios)
         Dim usuario As String = userTextBox.Text.ToLower
         Dim password As String = passwordTextBox.Text
         controller.VerificarLogin(usuario, password)
