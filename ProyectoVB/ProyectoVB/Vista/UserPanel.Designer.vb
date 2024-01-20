9aa7b5b69f082ed7c477693a26fb6ae5e811a6d7
IÑIGOR | DAM <inigo.retamero@ikasle.egibide.org>
2024-01-20 10:26:57 +0100

    RESIZE USER PANEL PERFECT

--- ProyectoVB/ProyectoVB/Vista/UserPanel.Designer.vb
+++ ProyectoVB/ProyectoVB/Vista/UserPanel.Designer.vb
@@ -43,6 +43,7 @@ Partial Class UserPanel
         Me.panelLayoutPanel.Controls.Add(Me.agendaBox, 1, 0)
         Me.panelLayoutPanel.Controls.Add(Me.contactoGroupBox, 0, 1)
         Me.panelLayoutPanel.Controls.Add(Me.controlUserButton, 1, 1)
+        Me.panelLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill
         Me.panelLayoutPanel.Location = New System.Drawing.Point(0, 0)
         Me.panelLayoutPanel.Margin = New System.Windows.Forms.Padding(2)
         Me.panelLayoutPanel.Name = "panelLayoutPanel"
@@ -52,7 +53,7 @@ Partial Class UserPanel
         Me.panelLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
         Me.panelLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
         Me.panelLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
-        Me.panelLayoutPanel.Size = New System.Drawing.Size(640, 349)
+        Me.panelLayoutPanel.Size = New System.Drawing.Size(639, 344)
         Me.panelLayoutPanel.TabIndex = 1
         '
         'agendaLabel
@@ -61,7 +62,7 @@ Partial Class UserPanel
         Me.agendaLabel.AutoSize = True
         Me.agendaLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.agendaLabel.ForeColor = System.Drawing.SystemColors.Window
-        Me.agendaLabel.Location = New System.Drawing.Point(41, 38)
+        Me.agendaLabel.Location = New System.Drawing.Point(41, 37)
         Me.agendaLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.agendaLabel.Name = "agendaLabel"
         Me.agendaLabel.Size = New System.Drawing.Size(408, 39)
@@ -75,7 +76,7 @@ Partial Class UserPanel
         Me.agendaBox.Location = New System.Drawing.Point(493, 2)
         Me.agendaBox.Margin = New System.Windows.Forms.Padding(2)
         Me.agendaBox.Name = "agendaBox"
-        Me.agendaBox.Size = New System.Drawing.Size(145, 112)
+        Me.agendaBox.Size = New System.Drawing.Size(144, 110)
         Me.agendaBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.agendaBox.TabIndex = 11
         Me.agendaBox.TabStop = False
@@ -86,22 +87,20 @@ Partial Class UserPanel
         Me.contactoGroupBox.Controls.Add(Me.contactoComboBox)
         Me.contactoGroupBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.contactoGroupBox.ForeColor = System.Drawing.SystemColors.Window
-        Me.contactoGroupBox.Location = New System.Drawing.Point(92, 118)
+        Me.contactoGroupBox.Location = New System.Drawing.Point(92, 116)
         Me.contactoGroupBox.Margin = New System.Windows.Forms.Padding(2)
         Me.contactoGroupBox.Name = "contactoGroupBox"
         Me.contactoGroupBox.Padding = New System.Windows.Forms.Padding(2)
-        Me.contactoGroupBox.Size = New System.Drawing.Size(306, 112)
+        Me.contactoGroupBox.Size = New System.Drawing.Size(306, 110)
         Me.contactoGroupBox.TabIndex = 19
         Me.contactoGroupBox.TabStop = False
         Me.contactoGroupBox.Text = "Contacto"
         '
         'contactoComboBox
         '
-        Me.contactoComboBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.contactoComboBox.Anchor = System.Windows.Forms.AnchorStyles.Left
         Me.contactoComboBox.FormattingEnabled = True
-        Me.contactoComboBox.Location = New System.Drawing.Point(61, 47)
+        Me.contactoComboBox.Location = New System.Drawing.Point(78, 46)
         Me.contactoComboBox.Margin = New System.Windows.Forms.Padding(2)
         Me.contactoComboBox.Name = "contactoComboBox"
         Me.contactoComboBox.Size = New System.Drawing.Size(188, 30)
@@ -110,7 +109,7 @@ Partial Class UserPanel
         'controlUserButton
         '
         Me.controlUserButton.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.controlUserButton.Location = New System.Drawing.Point(505, 153)
+        Me.controlUserButton.Location = New System.Drawing.Point(505, 150)
         Me.controlUserButton.Margin = New System.Windows.Forms.Padding(2)
         Me.controlUserButton.Name = "controlUserButton"
         Me.controlUserButton.Size = New System.Drawing.Size(120, 42)
@@ -126,7 +125,7 @@ Partial Class UserPanel
         Me.ClientSize = New System.Drawing.Size(639, 344)
         Me.Controls.Add(Me.panelLayoutPanel)
         Me.Margin = New System.Windows.Forms.Padding(2)
-        Me.MaximumSize = New System.Drawing.Size(655, 383)
+        Me.MinimumSize = New System.Drawing.Size(655, 383)
         Me.Name = "UserPanel"
         Me.Text = "AGENDA | USER PANEL"
         Me.panelLayoutPanel.ResumeLayout(False)
--- ProyectoVB/ProyectoVB/Vista/UserPanel.vb
+++ ProyectoVB/ProyectoVB/Vista/UserPanel.vb
@@ -2,9 +2,26 @@
     Dim controller As New Controller()
     Dim listaContactos As New List(Of Contacto)
 
+    ' Tamaños originales
+    Private Const tamañoOriginalX As Integer = 655
+    Private Const tamañoOriginalY As Integer = 383
+    Private Const tamañoFuenteOriginalAgendaLabel As Double = 25
+    Private Const tamañoFuenteOriginalcontrolUserButton As Double = 8
+    Private Const tamañoFuenteOriginalContactoGroupBox As Double = 13
+    Private Const tamañoFuenteOriginalComboBox As Double = 13
+
+    ' Escala de tamaño actual
+    Private escalaX As Double = 1.0
+    Private escalaY As Double = 1.0
+
     Private Sub UserPanel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Llamada al método para cargar los nombres de contactos en el ComboBox
         CargarNombresDeContactos()
+    End Sub
+
+    Private Sub UserPanel_SizeChanged(sender As Object, e As EventArgs) Handles Me.SizeChanged
+        ' Llamamos a la función para ajustar el tamaño
+        AjustarTamaño()
     End Sub
 
     ' Método para cargar los nombres de los contactos en el ComboBox
@@ -57,9 +74,24 @@
         userControl.Dock = DockStyle.Fill
     End Sub
 
-    Private Sub panelLayoutPanel_Paint(sender As Object, e As PaintEventArgs) Handles panelLayoutPanel.Paint
+    Private Sub AjustarTamaño()
+        ' Calcular la escala actual
+        escalaX = Me.Width / tamañoOriginalX
+        escalaY = Me.Height / tamañoOriginalY
 
+        ' Ajustar tamaños de fuente y elementos según la escala
+        agendaLabel.Font = New Font("Microsoft Sans Serif", CSng(tamañoFuenteOriginalAgendaLabel * escalaY), FontStyle.Regular, GraphicsUnit.Point, CType(0, Byte))
+        controlUserButton.Font = New Font("Microsoft Sans Serif", CSng(tamañoFuenteOriginalcontrolUserButton * escalaY), FontStyle.Regular, GraphicsUnit.Point, CType(0, Byte))
+        contactoGroupBox.Font = New Font("Microsoft Sans Serif", CSng(tamañoFuenteOriginalContactoGroupBox * escalaY), FontStyle.Regular, GraphicsUnit.Point, CType(0, Byte))
+        contactoComboBox.Font = New Font("Microsoft Sans Serif", CSng(tamañoFuenteOriginalComboBox * escalaY), FontStyle.Regular, GraphicsUnit.Point, CType(0, Byte))
+
+        ' Ajustar tamaños de elementos según la escala
+        agendaBox.Size = New Size(CInt(144 * escalaX), CInt(110 * escalaY))
+        controlUserButton.Size = New Size(CInt(120 * escalaX), CInt(42 * escalaY))
+        contactoGroupBox.Size = New Size(CInt(306 * escalaX), CInt(110 * escalaY))
+        contactoComboBox.Size = New Size(CInt(188 * escalaX), CInt(30 * escalaY))
     End Sub
 End Class
+
 
 
